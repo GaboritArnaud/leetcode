71286c8f76508a2c89038f847d35cbe5907736e5
arnaud <gaboritarnaud.contact@gmail.com>
2022-06-23 12:08:39 +0200

    feat(problem) : 136 Single Number refacto test

--- src/test/java/leetcode/easy/singlenumber/SolutionTest.java
+++ src/test/java/leetcode/easy/singlenumber/SolutionTest.java
@@ -2,32 +2,28 @@ package leetcode.easy.singlenumber;
 
 import fr.agaborit.leetcode.easy.singlenumber.Solution;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
 
-    @Test
-    void cas_1() {
-        var s = new Solution();
-        var input = new int[] {2, 2, 1};
-        var result = s.singleNumber(input);
-        assertThat(result).isEqualTo(1);
+    @ParameterizedTest
+    @MethodSource("provideParamsForLeetcodeExempleTest")
+    void leetcodeExempleTest(int[] input, int expected) {
+        assertThat(new Solution().singleNumber(input)).isEqualTo(expected);
     }
 
-    @Test
-    void cas_2() {
-        var s = new Solution();
-        var input = new int[] {4, 1, 2, 1, 2};
-        var result = s.singleNumber(input);
-        assertThat(result).isEqualTo(4);
-    }
 
-    @Test
-    void cas_3() {
-        var s = new Solution();
-        var input = new int[] {1};
-        var result = s.singleNumber(input);
-        assertThat(result).isEqualTo(1);
+    private static Stream<Arguments> provideParamsForLeetcodeExempleTest() {
+        return Stream.of(
+                Arguments.of(new int[] {2, 2, 1}, 1),
+                Arguments.of(new int[] {4, 1, 2, 1, 2}, 4),
+                Arguments.of(new int[] {1}, 1)
+        );
     }
-
 }
